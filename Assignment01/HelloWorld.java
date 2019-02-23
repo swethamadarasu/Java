7aaa059014b1508572df1847ba5b1dbbe61b71a9
swethamadarasu <35619404+swethamadarasu@users.noreply.github.com>
2019-02-23 11:48:40 -0600

    Add files via upload

---
+++ Assignment01/HelloWorld.java
@@ -0,0 +1,26 @@
+/************************************************************
+ *                                                          *
+ *  CSCI 470/502          Assignment 1           Fall 2018  *                                             
+ *                                                          *
+ *  Programmer: Lakshmi Swetha Madarasu                     *  
+ *                                                          *
+ *  Date Due:   08/31/2018                                  *                          
+ *                                                          *
+ *  Purpose:    First Java Program. Printing Hello, World!  *
+ *              Getting familiar with Dr. Java              *
+ *                                                          * 
+ ***********************************************************/ 
+
+public class HelloWorld 
+  
+{
+  
+  public static void main(String[] args) 
+    
+  { 
+    
+    System.out.println("Hello, World!");
+    
+  }
+  
+}
